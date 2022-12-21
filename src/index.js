26a67eff5cd448a6486263b1eed148437ed9b3f7
ashish <ashishroy7652@gmail.com>
2022-12-22 04:31:55 +0530

    added basename

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ console.log("store ", store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode basename={routerBaseName}>
-    <BrowserRouter >
+  <React.StrictMode >
+    <BrowserRouter basename={routerBaseName}>
     <Provider store={store} >
       <App />
     </Provider>
