1b44c5d073073943c499d4fe9446fb1e273bde32
ashish <ashishroy7652@gmail.com>
2022-12-22 04:02:01 +0530

    changes basename

--- src/components/today/TopNav.jsx
+++ src/components/today/TopNav.jsx
@@ -1,21 +1,22 @@
 import React from 'react' ; 
 import { Link } from 'react-router-dom';
 import styles from '../../styles/topnav.module.css' ; 
+const routerBaseName = process.env.PUBLIC_URL;
 
 const TopNav = () => {
   return (
     <div className={styles.topnav}>   
         <ul>
           <li>
-            <Link className={styles.decorationLogo} to='/'> Habit Tracker React </Link>
+            <Link basename={routerBaseName} className={styles.decorationLogo} to='/'> Habit Tracker React </Link>
           </li>
 
           <li>
-            <Link className={styles.decoration} to='/'>Home  </Link>
+            <Link basename={routerBaseName} className={styles.decoration} to='/'>Home  </Link>
           </li>
 
           <li>
-            <Link className={styles.decoration} to='/weekly'> Weekly </Link>
+            <Link basename={routerBaseName} className={styles.decoration} to='/weekly'> Weekly </Link>
           </li>
         </ul>
     </div>
