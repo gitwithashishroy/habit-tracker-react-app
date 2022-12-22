d02ac78194aab77f6625f6e7d13baca11c8515bd
ashish <ashishroy7652@gmail.com>
2022-12-22 18:21:36 +0530

    added basename in browser router

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode >
-    <BrowserRouter >
+    <BrowserRouter basename='/habit-tracker-react-app'>
     <Provider store={store} >
       <App />
     </Provider>
