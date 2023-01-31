cac276bc200499d8687b9d489ff0503da58e4985
ashish <ashishroy7652@gmail.com>
2023-02-01 00:45:13 +0530

    done

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode >
-    <BrowserRouter basename='/habit-tracker-react-app'>
+    <BrowserRouter >
     <Provider store={store} >
       <App />
     </Provider>
